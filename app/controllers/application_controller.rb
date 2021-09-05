bee4681769a346aac08e13cbbed5668c6611aba2
Toyoda Yoshiteru <6089more@gmail.com>
2021-09-05 11:39:21 +0000

    [add] gem devise ログイン機能、新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters,if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "正常にログインできました！いい出会いを＾＾"
+      homes_about_path
+    end
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:name,:age,:adderss,:profile_image_id,
+    :sex,:playing_game,:introduction])
+  end
 end
