f7a84e15619a82052f835569d17d0ce52b3a9541
Toyoda Yoshiteru <6089more@gmail.com>
2021-09-06 10:07:18 +0000

    [add] user_page（編集、一覧、詳細ページ）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,44 @@
 class UsersController < ApplicationController
   def index
+   #フォロ―ランキング上位4位の方を表示したい
+   # @users_ranking = User.where.not(id: current_user.id).page(params[:page]).per(8).order("")
+
+   #自分以外のユーザーのデータを取得、降順にしておりかみなりで24件ごとにページを作る
+  #@users = User.where.not(id: current_user.id).page(params[:page]).per(24).order("created_at DESC")
+  @users = User.all
   end
 
   def show
+      @user = User.find(params[:id])
   end
 
   def edit
+      @user = User.find(params[:id])
   end
 
   def update
+      user = User.find(params[:id])
+      if user.update(user_params)
+          redirect_to user_path(user)
+      end
   end
-  
+
   def withdraw
+      @user = current_user
+      @user.update(is_deleted: true)
+      reset_session
+      flash[:notice] = "またのご利用を心より願っています(^^ゞ"
+      redirect_to root_path
   end
-  
+
   def unsubscribe
+      @user = current_user
   end
-  
+
+ private
+
+  def user_params
+      params.require(:user).permit(:name,:age,:introduction,:address,:playing_game,:profile_image)
+  end
+
 end
