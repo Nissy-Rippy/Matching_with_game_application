a31875cfba652078a1b4d55a07e8929d6d0e92ed
Toyoda Yoshiteru <6089more@gmail.com>
2021-09-06 17:22:26 +0000

    {add] postの一覧、詳細、新規投稿ぺージの作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+  class PostsController < ApplicationController
+    
+    def index
+      @posts = Post.all
+    end
+  
+    def new
+      @post = Post.new
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+    end
+  
+    def create
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
+      if @post.save
+        flash[:notice] = "投稿完了です＾＾言い値が沢山もらえますように💛"
+        redirect_to posts_path
+      else
+        flash.now[:notice] = "記入ミスの可能性あり～(￣ー￣)ﾆﾔﾘ"
+        render :new
+      end
+    end
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      flash[:notice] = "投稿を削除しちゃった(*´σｰ｀)ｴﾍﾍ"
+      redirect_to 
+    end
+  private
+  
+    def post_params
+      params.require(:post).permit(:description,:post_title,:image)
+    end
+    
+  end
