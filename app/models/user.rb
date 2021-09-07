b2847a059c69eb84aa6b44bd92cafad2226c882b
Toyoda Yoshiteru <6089more@gmail.com>
2021-09-07 23:24:43 +0000

    [add] follow_functionフォロー、フォロワー機能追加非同期通信済み

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   
+  #フォローするuser側からみて、フォローされる側のユーザーを集める。そのため外部キーはfollowing_idとなっている。
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
+  #中間テーブルをthroughで通してfollowerモデルのユーザーを集めたモデルを、followingsと名付けた。
+  has_many :followings, through: :active_relationships, source: :follower
+  #フォローされる側から見て、フォロー下側のユーザーを集める。そのため外部キーはfollower_idとなっている。
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  #中間テーブルを通して,followedモデルのユーザーを集めたモデルをfollowerと名づけた。
+  has_many :followers, through: :passive_relationships, source: :following
   
   
   attachment :profile_image
@@ -19,4 +27,8 @@ class User < ApplicationRecord
     super && is_deleted == false
   end
     
+  def followed_by?(user)
+    #現ユーザーがこの人をフォローしているかどうか確かめるためのコード
+   passive_relationships.find_by(following_id: user.id).present?
+  end
 end
