e4e7a64a4065e0a3b23ebcc482e8521b6ba989a9
Toyoda Yoshiteru <6089more@gmail.com>
2021-09-14 11:48:16 +0000

    [fix] migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_123600) do
+ActiveRecord::Schema.define(version: 2021_09_14_114235) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
@@ -92,15 +92,16 @@ ActiveRecord::Schema.define(version: 2021_09_12_123600) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "taggings", force: :cascade do |t|
+  create_table "tag_links", force: :cascade do |t|
     t.integer "tag_id"
     t.string "taggable_type"
     t.integer "taggable_id"
     t.string "tagger_type"
     t.integer "tagger_id"
     t.string "context", limit: 128
-    t.datetime "created_at"
+    t.datetime "created_at", null: false
     t.string "tenant", limit: 128
+    t.datetime "updated_at", null: false
     t.index ["context"], name: "index_taggings_on_context"
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
@@ -111,14 +112,6 @@ ActiveRecord::Schema.define(version: 2021_09_12_123600) do
     t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
     t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
     t.index ["tenant"], name: "index_taggings_on_tenant"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "user_rooms", force: :cascade do |t|
