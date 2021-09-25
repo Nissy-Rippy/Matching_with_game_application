75e95ab6040c9960fca69e0a1f4605afeb6c32c6
Toyoda Yoshiteru <6089more@gmail.com>
2021-09-25 12:41:54 +0000

    [fix] login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
 
   def after_sign_in_path_for(resource)
-    homes_about_path
     flash[:notice] = "正常にログインできました！いい出会いを＾＾"
+    homes_about_path
   end
 
   def after_sign_out_path_for(resource)
